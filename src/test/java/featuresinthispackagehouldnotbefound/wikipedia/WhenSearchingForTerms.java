090a73e9962eb49716476e1f2e9a60d73a8a43f7
gallardo <argrico@gmail.com>
2022-04-26 11:24:05 +0200

    Simplify code to isolate and reproduce issue
    This code is purposely missing the serenity configuration `serenity.test.root` and should consequently not allow serenity-bdd to build a report with the tabs "features" and "stories"

---
+++ src/test/java/featuresinthispackagehouldnotbefound/wikipedia/WhenSearchingForTerms.java
@@ -0,0 +1,14 @@
+package featuresinthispackagehouldnotbefound.wikipedia;
+
+import net.serenitybdd.junit5.SerenityJUnit5Extension;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(SerenityJUnit5Extension.class)
+class WhenSearchingForTerms {
+
+    @Test
+    void searchBySingleKeyword() {
+    }
+}
